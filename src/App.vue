7d4b54b00ce514b2652e868db70c9ce95d524ff2
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-09-23 22:00:05 +0300

    app: Add `leaflet` and `vue-leaflet`

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 12,
+    };
+  },
+};
+</script>
 
 <template>
   <h1 class="text-3xl font-bold">Pyöräparkit kartalla</h1>
+  <div style="height: 600px; width: 800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[61.49911, 23.78712]" :useGlobalLeaflet="false">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+        :attribution="attribution"
+      ></l-tile-layer>
+    </l-map>
+  </div>
 </template>
