7719f4606bb0475d978ebe9cff2a43b8d8b001d4
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-09-30 19:27:40 +0300

    app: Generate tampere data, add attribution

--- src/views/AppMenu/AppInfo.vue
+++ src/views/AppMenu/AppInfo.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+import ExternalLink from '../../components/ExternalLink.vue';
+import { tampereParkingSpotsUpdatedAt } from '../../data/tampere';
+</script>
+
 <template>
   <div class="flex flex-col gap-4">
-    <h2>{{ $t('tabs.info') }}</h2>
+    <h2>{{ $t('info.dataSources.header') }}</h2>
+
+    <h3>Tampere</h3>
+    <p>
+      <i18next :translation="$t('info.dataSources.tampere.attribution')">
+        <template #sourceLink>
+          <ExternalLink
+            href="https://data.tampere.fi/data/dataset/tampereen-pyoraparkit"
+            text="Tampereen pyöräparkit"
+          />
+        </template>
+        <template #sourceServiceLink>
+          <ExternalLink href="https://data.tampere.fi/" text="Tampereen kaupungin dataportaali" />
+        </template>
+        <template #licenseLink>
+          <ExternalLink
+            href="https://creativecommons.org/licenses/by/4.0/"
+            text="Creative Commons Attribution 4.0"
+          />
+        </template>
+        <template #date>
+          {{ tampereParkingSpotsUpdatedAt }}
+        </template>
+      </i18next>
+    </p>
   </div>
 </template>
--- src/views/AppMenu/ClosestParkingSpots.vue
+++ src/views/AppMenu/ClosestParkingSpots.vue
@@ -3,56 +3,12 @@ import ParkingSpotList from '../../components/ParkingSpotList/ParkingSpotList.vu
 import ParkingSpotListItem, {
   type ParkingSpotListItemProps,
 } from '../../components/ParkingSpotList/ParkingSpotListItem.vue';
+import tampereParkingSpots from '../../data/tampere';
 
-const spots: ParkingSpotListItemProps[] = [
-  {
-    spot: {
-      area: 'Nalkala',
-      coordinates: { lat: 1, lon: 2 },
-      id: 'tre:223',
-      rack: {
-        count: 2,
-        frameLock: false,
-        manufacturer: 'Finture',
-        model: 'Perinteinen',
-        spots: 16,
-      },
-      status: 'IN_USE',
-      type: 'BIKE_PARKING',
-      image: 'https://kartat.tampere.fi/valokuvia/isot_sensuroitu/223.JPG',
-    },
-    distance: 100,
-  },
-  {
-    spot: {
-      area: 'Kaakinmaa',
-      coordinates: { lat: 1, lon: 2 },
-      id: 'tre:267',
-      rack: {
-        count: 2,
-        frameLock: false,
-        manufacturer: 'Finture',
-        model: 'Perinteinen',
-        spots: 10,
-      },
-      status: 'IN_USE',
-      type: 'CONNECTION_PARKING',
-      image: 'https://kartat.tampere.fi/valokuvia/isot_sensuroitu/267.JPG',
-    },
-    distance: 100,
-  },
-  {
-    spot: {
-      area: 'Ratina',
-      coordinates: { lat: 1, lon: 2 },
-      id: 'tre:11',
-      rack: { count: 6, frameLock: true, manufacturer: 'Alushel', model: 'BAR-L60', spots: 12 },
-      status: 'NOT_IN_USE',
-      type: 'BIKE_PARKING',
-    },
-    distance: 100,
-  },
-];
+const spots: ParkingSpotListItemProps[] = tampereParkingSpots.map((spot) => ({
+  spot,
+  distance: 100,
+}));
 </script>
 
 <template>
