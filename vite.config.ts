de54cc73fcf6b11119463c067be7f8426d200b9a
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-10-05 16:53:34 +0300

    app: Add `pwa` configuration, cache osm tiles

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,68 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pyoraparkit-kartalla/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Pyöräparkit kartalla',
+        short_name: 'Pyöräparkit kartalla',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/pyoraparkit-kartalla/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pyoraparkit-kartalla/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pyoraparkit-kartalla/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pyoraparkit-kartalla/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        theme_color: '#aceb98',
+        background_color: '#aceb98',
+        display: 'standalone',
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.\.tile\.openstreetmap\.org\/\d+\/\d+\/\d+.png/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'osm-cache',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
