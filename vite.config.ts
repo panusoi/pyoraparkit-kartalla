42b560ca2aefc36e2c80ecfdffcddc8358a11877
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-10-05 16:59:35 +0300

    app: Add `@vitejs/plugin-basic-ssl` and `preview:ssl` to make mobile testing easier

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
+import basicSSL from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -63,6 +64,7 @@ export default defineConfig({
         ],
       },
     }),
+    process.env.LOCAL_SSL ? basicSSL() : undefined,
   ],
   resolve: {
     alias: {
