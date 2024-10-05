ae4e133bab9cd5a4c259e0303e3345ae746156f3
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-10-05 12:05:53 +0300

    app: Fix map not moving to current location on load

--- src/composables/useMap.ts
+++ src/composables/useMap.ts
@@ -1,5 +1,12 @@
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import type { LatLng } from '../types/location';
+import useGeolocation from './useGeolocation';
+
+let isLocationCentered = false;
+
+const { location } = useGeolocation();
+
+const mapReady = ref<boolean>(false);
 
 const center = ref<[number, number]>(
   [61.49911, 23.78712], // Tampere
@@ -7,11 +14,32 @@ const center = ref<[number, number]>(
 
 const focus = ref<[number, number] | null>(null);
 
-function setFocus(coordinates: LatLng): void {
+function setCenter(coordinates: LatLng): void {
   center.value = [coordinates.lat, coordinates.lng];
+}
+
+function setFocus(coordinates: LatLng): void {
+  setCenter(coordinates);
   focus.value = [coordinates.lat, coordinates.lng];
 }
 
+function setMapReady(map: unknown) {
+  mapReady.value = true;
+  // Exposes Leaflet map object to window, used in tests
+  (window as unknown as { __LEAFLET_MAP__: unknown }).__LEAFLET_MAP__ = map;
+}
+
+watchEffect(() => {
+  if (
+    location.value.status === 'current' &&
+    mapReady.value === true &&
+    isLocationCentered === false
+  ) {
+    setCenter(location.value);
+    isLocationCentered = true;
+  }
+});
+
 export default function useMap() {
-  return { center, focus, setFocus };
+  return { center, focus, setCenter, setFocus, setMapReady };
 }
