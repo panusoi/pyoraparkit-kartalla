a0a80900c091adbbb5a9d5ce63ca1a5ff00531af
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2024-10-07 21:50:45 +0300

    app: Add transition to menu and spot details

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -3,6 +3,8 @@ import useMenu from '../composables/useMenu';
 import useParkingSpotDetails from '../composables/useParkingSpotDetails';
 const { isMenuOpen } = useMenu();
 const { isOpen: isParkingSpotDetailsOpen } = useParkingSpotDetails();
+const menuClass =
+  'absolute inset-y-0 z-10 w-full bg-primary-light-50 p-4 text-primary-light-600 md:m-4 md:w-96 md:rounded-xl md:shadow-lg dark:bg-primary-dark-50 dark:text-primary-dark-600';
 </script>
 
 <template>
@@ -11,22 +13,28 @@ const { isOpen: isParkingSpotDetailsOpen } = useParkingSpotDetails();
       <div class="z-0 h-full w-full">
         <slot name="map"></slot>
       </div>
-      <div
-        :class="[
-          isMenuOpen && !isParkingSpotDetailsOpen ? 'visible' : 'collapse',
-          'absolute inset-y-0 left-0 z-10 w-full bg-primary-light-50 p-4 text-primary-light-600 md:m-4 md:w-96 md:rounded-xl md:shadow-lg dark:bg-primary-dark-50 dark:text-primary-dark-600',
-        ]"
+      <transition
+        enter-active-class="transition-opacity duration-150"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100 "
+        leave-active-class="transition-opacity duration-150"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
       >
-        <slot name="menu"></slot>
-      </div>
-      <div
-        :class="[
-          isParkingSpotDetailsOpen ? 'visible' : 'collapse',
-          'absolute inset-y-0 right-0 z-10 w-full bg-primary-light-50 p-4 text-primary-light-600 md:m-4 md:w-96 md:rounded-xl md:shadow-lg dark:bg-primary-dark-50 dark:text-primary-dark-600',
-        ]"
+        <div v-if="isMenuOpen && !isParkingSpotDetailsOpen" :class="[menuClass, 'left-0']">
+          <slot name="menu"></slot></div
+      ></transition>
+      <transition
+        enter-active-class="transition-opacity duration-150"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition-opacity duration-150"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
       >
-        <slot name="parking-spot-details"></slot>
-      </div>
+        <div v-if="isParkingSpotDetailsOpen" :class="[menuClass, 'right-0']">
+          <slot name="parking-spot-details"></slot></div
+      ></transition>
     </div>
   </div>
 </template>
